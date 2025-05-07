0114f14ac94088622aaaeabefd2eaa415e02c15c
HACKER-HROF <hfhr39@gmail.com>
2025-05-07 13:01:46 +0300

    Update HROFF.py

--- HROFF.py
+++ HROFF.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://mdserge.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://popstool.io/api"
 
 class MdSerge:
     def __init__(self, access_key) -> None:
