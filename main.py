b42e57b4042f6dfb79bb4fdc6473c59289b76031
HACKER-HROF <hfhr39@gmail.com>
2025-05-09 09:49:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ def gradient_text(text, colors):
 
 def banner(console):
     os.system('cls' if os.name == 'nt' else 'clear')
-    brand_name = figlet_format('FLANKERTOOL', font='big')
+    brand_name = figlet_format('HROF', font='big')
     colors = [
         "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
     ]
