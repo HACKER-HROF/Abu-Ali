17b2c24e551ca636294a0a63c133cd4740d5ff6e
HACKER-HROF <hfhr39@gmail.com>
2025-05-09 09:41:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -253,10 +253,10 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     while True:
         banner(console)
-        acc_email = prompt_valid_value("[bold][?] Account Email[/bold]", "Email", password=False)
-        acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
-        acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
-        console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
+        acc_email = prompt_valid_value("[bold][?] ﻲﻧﻭﺮﺘﻜﻟﻹﺍ ﺏﺎﺴﺤﻟﺍ ﺪﻳﺮﺑ[/bold]", "Email", password=False)
+        acc_password = prompt_valid_value("[bold][?] ﺏﺎﺴﺤﻟﺍ ﺭﻭﺮﻣ ﺔﻤﻠﻛ[/bold]", "Password", password=False)
+        acc_access_key = prompt_valid_value("[bold][?] ﻝﻮﺻﻮﻟﺍ ﺡﺎﺘﻔﻣ[/bold]", "Access Key", password=False)
+        console.print("[bold cyan][%] ﻝﻮﺧﺪﻟﺍ ﻞﻴﺠﺴﺗ ﺔﻟﻭﺎﺤﻣ[/bold cyan]: ", end=None)
         cpm = CarParkTool(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
