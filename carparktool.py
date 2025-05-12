b843f45d1737ab141afcaf8d8dde9d0653df0fb0
HACKER-HROF <hfhr39@gmail.com>
2025-05-12 19:18:30 +0300

    Update carparktool.py

--- carparktool.py
+++ carparktool.py
@@ -3,7 +3,7 @@ from time import sleep
 
 BASE_URL: str = "https://popstool.io/api"
 
-class CarParkTool:
+class Egypt:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
